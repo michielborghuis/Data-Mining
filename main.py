fixed CV leakage

main.py
@@ -1,5 +1,3 @@
-from data_loading import ReviewLoader
-from preprocessing import ReviewProcessor
 from models import NaiveBayesClassifier,RandomForestClassifier
 
 import numpy as np
@@ -10,30 +8,32 @@ np.random.seed(42)
 random.seed(42)
 
 def main() -> None:
-    loader = ReviewLoader()
-    processor = ReviewProcessor()
-
-    # Load reviews from files
-    train_reviews, train_labels = loader.load_train_reviews()
-
-    # Preprocess reviews to convert to count matrix
-    train_X = processor.process_train_reviews(train_reviews, include_bigrams=False)
-
-    # Remove features (uni-/bigrams) that occur in very few documents
-    train_X = processor.filter_rare_terms(train_X, min_review_freq=.005)
-
-    print(train_X.shape)
-    #exit()
-
     # Specify which model configurations to evaluate
     models = [
-        #NaiveBayesClassifier(name="NaiveBayes(alpha=2)", smoothing_alpha=2.0),
-        #NaiveBayesClassifier(name="NaiveBayes(alpha=1)", smoothing_alpha=1.0),
-        #NaiveBayesClassifier(name="NaiveBayes(alpha=.1)", smoothing_alpha=0.1),
-        #NaiveBayesClassifier(name="NaiveBayes(alpha=1e-5)", smoothing_alpha=1e-5),
-        #RandomForestClassifier(name="RandomForest(n=50)", n_estimators=50),
-        RandomForestClassifier(name="RandomForest(n=100)", n_estimators=100),
-        #RandomForestClassifier(name="RandomForest(n=200)", n_estimators=200),
+        #NaiveBayesClassifier(name="NaiveBayes(1952)", smoothing_alpha=1.0, n_features=train_X.shape[1]) # Uses all features
+        #NaiveBayesClassifier(name="NaiveBayes(1933)", smoothing_alpha=1.0, n_features=1933), # Best unigram configuration (df_min = .005)
+        #NaiveBayesClassifier(name="NaiveBayes(bigram, 3436)", smoothing_alpha=0.5, n_features=3436), # Best unigram+bigram configuration (df_min = .010)
+        NaiveBayesClassifier(
+            smoothing_alpha=1.0,
+            min_df=.005,
+            drop_features=19,
+            include_bigrams=False,
+            name="NB ALL FEATURES"
+        ),
+        NaiveBayesClassifier(
+            smoothing_alpha=1.0,
+            min_df=.005,
+            drop_features=19,
+            include_bigrams=False,
+            name="NB 1933 FEATURES"
+        ),
+        NaiveBayesClassifier(
+            smoothing_alpha=1.0,
+            min_df=.005,
+            drop_features=122,
+            include_bigrams=False,
+            name="NB DROP 122 FEATS"
+        )
     ]
 
     print("")
@@ -43,23 +43,38 @@ def main() -> None:
     print(f"MODEL\t\t\t|\tACC\t|\tPREC\t|\tREC\t|\tF1")
     print("-"*100)
     for model in models:
-        cv_performance = model.get_validation_performance(train_X, train_labels, n_features=1250)
+        val_performance = model.get_validation_performance(n_folds=10, n_repeats=1)
+
         print(
             f"{model.name}\t|\t" + \
-            f"{100*cv_performance['accuracy']:.2f}%\t|\t" + \
-            f"{cv_performance['precision']:.2f}\t|\t" + \
-            f"{cv_performance['recall']:.2f}\t|\t" + \
-            f"{cv_performance['f1']:.2f}"
+            f"{100*np.mean(val_performance['accuracy']):.2f}%\t|\t" + \
+            f"{np.mean(val_performance['precision']):.2f}\t|\t" + \
+            f"{np.mean(val_performance['recall']):.2f}\t|\t" + \
+            f"{np.mean(val_performance['f1']):.2f}"
         )
     print("")
 
-    #models[0].analyse_feature_importances(index_to_word_mapping=processor.index_token_dict)
 
     # NOTE: we should only start looking at test set performance in a couple of weeks or so
     #   -> modelling/hyperparameter choices should NOT be based on test set performance 
 
-    #test_reviews, test_labels = loader.load_test_reviews()
-    #test_X = processor.process_test_reviews(test_reviews, include_bigrams=False)
+    print("")
+    print("TEST SET PERFORMANCES:")
+    print("")
+    print("-"*100)
+    print(f"MODEL\t\t\t|\tACC\t|\tPREC\t|\tREC\t|\tF1")
+    print("-"*100)
+    for model in models:
+        test_set_performance = model.get_test_performance()
+        print(
+            f"{model.name}\t|\t" + \
+            f"{100*test_set_performance['accuracy']:.2f}%\t|\t" + \
+            f"{test_set_performance['precision']:.3f}\t|\t" + \
+            f"{test_set_performance['recall']:.3f}\t|\t" + \
+            f"{test_set_performance['f1']:.3f}"
+        )
+    
+    models[0].analyse_feature_importances()
 
 if __name__ == "__main__":
     main()

models.py
@@ -6,17 +6,24 @@ from sklearn.naive_bayes import MultinomialNB
 from sklearn.ensemble import RandomForestClassifier as SklearnRF
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, mutual_info_score
 
-from util import CVManager
+from data_loading import ReviewLoader
+from preprocessing import ReviewProcessor
+from util import CVManager, update_index_token_dict
 
 class Classifier:
-    def __init__(self, name: str) -> None:
+    def __init__(self, min_df: float=.005, include_bigrams: bool=False, name: str="ReviewClassifier") -> None:
+        self.min_df = min_df
+        self.include_bigrams = include_bigrams
         self.name = name
+
+        self.loader = ReviewLoader()
+        self.processor = ReviewProcessor()
 
     def _initialize_model(self):
         # Model not defined for 'vanilla' classifier object
         self.model = None
 
-    def _get_performace_metrics(self, X: np.ndarray, y: np.ndarray) -> Dict[str,float]:
+    def get_performance_metrics(self, X: np.ndarray, y: np.ndarray) -> Dict[str,float]:
         predictions = self.model.predict(X)
 
         performance_dict = {}
@@ -27,98 +34,176 @@ class Classifier:
 
         return performance_dict
 
-    def get_validation_performance(self, X: np.ndarray, y: np.ndarray, n_features: int=None) -> Dict[str,float]:
-        cv_manager = CVManager(X=X, y=y, n_folds=10)
+    def get_validation_performance(self, n_folds: int=10, n_repeats: int=1) -> Dict[str,float]:
+        # Load reviews from files
+        reviews, labels = self.loader.load_train_reviews()
 
         performance_dict = {
-            "accuracy":[],
-            "precision":[],
-            "recall":[],
-            "f1":[]
+            "accuracy": np.zeros((n_repeats, n_folds)),
+            "precision": np.zeros((n_repeats, n_folds)),
+            "recall": np.zeros((n_repeats, n_folds)),
+            "f1": np.zeros((n_repeats, n_folds))
         }
 
-        for fold_nr in range(cv_manager.n_folds):
-            (train_X, train_y), (val_X, val_y) = cv_manager.get_fold_data()
+        for rep_i in range(n_repeats):
+            cv_manager = CVManager(reviews=reviews, labels=labels, n_folds=n_folds)
 
-            ###     START FEATURE SELECTION SECTION         ###
-            
-            ### CURRENT FEATURE UTILITY: MUTUAL INFORMATION ###
+            for fold_nr in range(cv_manager.n_folds):
+                (train_X, train_y), (val_X, val_y) = cv_manager.get_fold_data()
 
-            if n_features not in [train_X.shape[1], None]:
-                binary_X = (train_X > 0)*1
+                # Preprocess reviews to convert to count matrix
+                train_X = self.processor.process_train_reviews(train_X, include_bigrams=self.include_bigrams)
 
-                #doc_freqs = np.sum(binary_X/binary_X.shape[0], axis=0)
+                # Remove features (uni-/bigrams) that occur in very few documents
+                train_X = self.processor.filter_rare_terms(train_X, min_review_freq=self.min_df)
 
-                mi_scores = np.zeros(binary_X.shape[1])
-                for feature_idx in range(binary_X.shape[1]):
-                    mi_scores[feature_idx] = mutual_info_score(train_X[:,feature_idx], train_y)
+                # Load test reviews
+                val_X = self.processor.process_test_reviews(val_X, include_bigrams=self.include_bigrams)
 
-                feature_score = mi_scores#*doc_freqs
-                good_feature_ids = np.argpartition(feature_score, -n_features)[-n_features:]
+                self._initialize_model()
+                self.model.fit(train_X, train_y)
 
-                train_X = train_X[:,good_feature_ids]
-                val_X = val_X[:,good_feature_ids]
+                fold_metrics = self.get_performance_metrics(val_X, val_y)
 
-            ### END FEATURE SELECTION SECTION ###
-
-            self._initialize_model()
-            self.model.fit(train_X, train_y)
-
-            fold_metrics = self._get_performace_metrics(val_X, val_y)
-
-            performance_dict['accuracy'].append(fold_metrics['accuracy'])
-            performance_dict['precision'].append(fold_metrics['precision'])
-            performance_dict['recall'].append(fold_metrics['recall'])
-            performance_dict['f1'].append(fold_metrics['f1'])
-
-        performance_dict['accuracy'] = np.mean(performance_dict['accuracy'])
-        performance_dict['precision'] = np.mean(performance_dict['precision'])
-        performance_dict['recall'] = np.mean(performance_dict['recall'])
-        performance_dict['f1'] = np.mean(performance_dict['f1'])
+                performance_dict['accuracy'][rep_i][fold_nr] = fold_metrics['accuracy']
+                performance_dict['precision'][rep_i][fold_nr] = fold_metrics['precision']
+                performance_dict['recall'][rep_i][fold_nr] = fold_metrics['recall']
+                performance_dict['f1'][rep_i][fold_nr] = fold_metrics['f1']
 
         return performance_dict
+    
+    def get_test_performance(self) -> Dict[str,float]:
+        train_reviews, train_y = self.loader.load_train_reviews()
+
+        train_X = self.processor.process_train_reviews(train_reviews, include_bigrams=self.include_bigrams)
+        train_X = self.processor.filter_rare_terms(train_X, min_review_freq=self.min_df)
+
+        test_reviews, test_y = self.loader.load_test_reviews()
+        test_X = self.processor.process_test_reviews(test_reviews, include_bigrams=self.include_bigrams)
+
+        self._initialize_model()
+        self.model.fit(train_X, train_y)
+
+        return self.get_performance_metrics(test_X, test_y)
 
 class NaiveBayesClassifier(Classifier):
-    def __init__(self, smoothing_alpha: float=1.0, name: str="NaiveBayes") -> None:
-        super().__init__(name)
+    def __init__(self, smoothing_alpha: float=1.0, drop_features: int=0, min_df: float=.005, include_bigrams: bool=False, name: str="NaiveBayes") -> None:
+        super().__init__(min_df=min_df, include_bigrams=include_bigrams, name=name)
         self.alpha = smoothing_alpha
+        self.min_df = min_df
+        self.drop_features = drop_features
+        self.include_bigrams = include_bigrams
 
     def _initialize_model(self) -> None:
         self.model = MultinomialNB(alpha=self.alpha, force_alpha=True)
 
-    def analyse_feature_importances(self, index_to_word_mapping: Dict[int,str]) -> None:
+    def select_features(self, X: np.ndarray, y: np.ndarray) -> np.ndarray:
+        # Return indices of features that should be used
+        if self.drop_features in [None, 0]:
+            self.feature_indices = np.arange(X.shape[1])
+        elif self.drop_features > X.shape[1]:
+            raise ValueError(
+                f"Total number of possible features {X.shape[1]} exceeds " + \
+                f"specified number of features to drop {self.drop_features}"
+            )
+        else:
+            binary_X = (X > 0)*1
+
+            mi_scores = np.zeros(binary_X.shape[1])
+            for feature_idx in range(binary_X.shape[1]):
+                mi_scores[feature_idx] = mutual_info_score(binary_X[:,feature_idx], y)
+
+            feature_score = mi_scores
+            n_features = len(feature_score) - self.drop_features
+
+            self.feature_indices = np.argpartition(feature_score, -n_features)[-n_features:]
+        
+    def get_validation_performance(self, n_folds: int=10, n_repeats: int=1) -> Dict[str,float]:
+        # Load reviews from files
+        reviews, labels = self.loader.load_train_reviews()
+
+        performance_dict = {
+            "accuracy": np.zeros((n_repeats, n_folds)),
+            "precision": np.zeros((n_repeats, n_folds)),
+            "recall": np.zeros((n_repeats, n_folds)),
+            "f1": np.zeros((n_repeats, n_folds))
+        }
+
+        for rep_i in range(n_repeats):
+            cv_manager = CVManager(reviews=reviews, labels=labels, n_folds=n_folds)
+
+            for fold_nr in range(cv_manager.n_folds):
+                (train_X, train_y), (val_X, val_y) = cv_manager.get_fold_data()
+
+                # Preprocess reviews to convert to count matrix
+                train_X = self.processor.process_train_reviews(train_X, include_bigrams=self.include_bigrams)
+
+                # Remove features (uni-/bigrams) that occur in very few documents
+                train_X = self.processor.filter_rare_terms(train_X, min_review_freq=self.min_df)
+
+                # Load test reviews
+                val_X = self.processor.process_test_reviews(val_X, include_bigrams=self.include_bigrams)
+
+                self.select_features(train_X, train_y)
+
+                train_X = train_X[:,self.feature_indices]
+                val_X = val_X[:,self.feature_indices]
+
+                self._initialize_model()
+                self.model.fit(train_X, train_y)
+
+                fold_metrics = self.get_performance_metrics(val_X, val_y)
+
+                performance_dict['accuracy'][rep_i][fold_nr] = fold_metrics['accuracy']
+                performance_dict['precision'][rep_i][fold_nr] = fold_metrics['precision']
+                performance_dict['recall'][rep_i][fold_nr] = fold_metrics['recall']
+                performance_dict['f1'][rep_i][fold_nr] = fold_metrics['f1']
+
+        return performance_dict
+    
+    def get_test_performance(self) -> Dict[str,float]:
+        train_reviews, train_y = self.loader.load_train_reviews()
+
+        train_X = self.processor.process_train_reviews(train_reviews, include_bigrams=self.include_bigrams)
+        train_X = self.processor.filter_rare_terms(train_X, min_review_freq=self.min_df)
+
+        test_reviews, test_y = self.loader.load_test_reviews()
+        test_X = self.processor.process_test_reviews(test_reviews, include_bigrams=self.include_bigrams)
+
+        self.select_features(train_X, train_y)
+
+        train_X = train_X[:,self.feature_indices]
+        test_X = test_X[:,self.feature_indices]
+
+        self._initialize_model()
+        self.model.fit(train_X, train_y)
+
+        return self.get_performance_metrics(test_X, test_y)
+
+    def analyse_feature_importances(self) -> None:
+        feature_mapping = self.processor.index_token_dict
+        feature_mapping = update_index_token_dict(feature_mapping, self.feature_indices)
+
         log_probs = self.model.feature_log_prob_
-        probs = np.exp(log_probs)
-        prob_ratios = probs[1]/probs[0]
-        indices = np.argsort(prob_ratios)
-        max_len = np.max([len(token) for token in index_to_word_mapping.values()])
+        log_odds = log_probs[1] - log_probs[0]
 
-        print('MOST IMPORTANT FEATURES FOR NEGATIVE CLASSIFICATIONS:\n')
-        print('Token'.ljust(max_len + 2)+'|\tP(x|y=1)/P(x|y=0)')
-        print('-'*35)
-        for i in indices[:20]:
-            token = index_to_word_mapping[i]
-            print(f"{token}".ljust(max_len + 2)+f"|\t{prob_ratios[i]:.3f}")
+        # Top for true reviews
+        true_indices = np.argsort(log_odds)[::-1][:10]
 
-        print("")
-        print('#'*35)
-        print("")
+        print(f"\nTop 10 features for true reviews:")
+        for j in true_indices:
+            fn = feature_mapping[j]
+            lo = log_odds[j] 
+            print(f"  {fn:<25} log-odds={lo:+.4f}  ")
 
-        print('MOST IMPORTANT FEATURES FOR POSITIVE CLASSIFICATIONS:\n')
-        print('Token'.ljust(max_len + 2)+'|\tP(x|y=1)/P(x|y=0)')
-        print('-'*35)
-        for i in indices[-20:]:
-            token = index_to_word_mapping[i]
-            print(f"{token}".ljust(max_len + 2)+f"|\t{prob_ratios[i]:.3f}")
-
-        print("")
-        print('#'*35)
-        print("")
-
-        low_ratios = prob_ratios[np.where(prob_ratios < 1.25)]
-        moderate_ratios = low_ratios[np.where(low_ratios > .8)]
-
-        print(f"{len(moderate_ratios)}/{len(prob_ratios)} FEATURES APPEAR NON-DISCRIMINATIVE (0.8 < PROB RATIO < 1.25)")
+        # Top for fake reviews
+        fake_indices = np.argsort(log_odds)[:10]
+        
+        print(f"\nTop 10 features for fake reviews:")
+        for j in fake_indices:
+            fn = feature_mapping[j]
+            lo = log_odds[j] 
+            print(f"  {fn:<25} log-odds={lo:+.4f}  ")
 
 class RandomForestClassifier(Classifier):
     def __init__(self, n_estimators: int = 100, name: str = "RandomForest") -> None:

util.py
@@ -1,20 +1,20 @@
-from typing import Tuple
+from typing import Dict, Tuple, List
 
 import numpy as np
 
 class CVManager:
-    def __init__(self, X: np.ndarray, y: np.ndarray, n_folds: int=10) -> None:
+    def __init__(self, reviews: List[str], labels: np.ndarray, n_folds: int=10) -> None:
         self.n_folds = n_folds
 
         # Shuffle data
-        perm = np.random.permutation(X.shape[0])
-        X = X[perm]
-        y = y[perm]
+        perm = np.random.permutation(reviews.shape[0])
+        reviews = np.array(reviews)[perm]
+        labels = labels[perm]
 
         # Create folds using equal slices from shuffled data
-        idx_folds = np.array_split(np.arange(X.shape[0]), n_folds)
-        self.X_slices = [X[idx] for idx in idx_folds]
-        self.y_slices = [y[idx] for idx in idx_folds]
+        idx_folds = np.array_split(np.arange(reviews.shape[0]), n_folds)
+        self.X_slices = [reviews[idx] for idx in idx_folds]
+        self.y_slices = [labels[idx] for idx in idx_folds]
 
         self.cur_index = 0
 
@@ -31,3 +31,11 @@ class CVManager:
         self.cur_index += 1
 
         return (train_X, train_y), (val_X, val_y)
+    
+def update_index_token_dict(original: Dict[int,str], indices: List) -> List[str]:
+    token_list = ["PLACEHOLDER"] * (max(original.keys())+1)
+    for i in range(len(token_list)):
+        token_list[i] = original[i]
+    
+    token_array = np.array(token_list)
+    return list(token_array[indices])