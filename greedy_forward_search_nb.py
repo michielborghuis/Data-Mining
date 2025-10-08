fixed leakage

greedy_forward_search_nb.py
@@ -11,6 +11,7 @@ random.seed(42)
 def main() -> None:
     #print("UNIGRAM (continued, best=565)")
     print("BIGRAM")
+    #print("UNIGRAM")
     print("")
     print("CROSS-VALIDATION PERFORMANCES:")
     print("")
@@ -18,27 +19,31 @@ def main() -> None:
     print(f"MODEL\t\t|\tACC\t\tMAX ACC\t\t|\tSE")
     print("-"*30+" "*8+"-"*30)
 
-    drop_features = 0
-    step_size = 50
+    features = 143
+    step_size = 1
     all_accs = []
-    best_acc = 0
-    standard_error = 0
+    best_acc = .8312
+    standard_error = .03
+    stall_counter = 0
+    patience = 50
     while True:
         try:
             #model = NaiveBayesClassifier(
             #    smoothing_alpha=1.0,
             #    min_df=.005,
-            #    drop_features=drop_features,
+            #    features=features,
+            #    feature_mode="include",
             #    include_bigrams=False,
-            #    name=f"DROP {drop_features} FEATS"
+            #    name=f"{features} FEATURES"
             #)
 
             model = NaiveBayesClassifier(
                 smoothing_alpha=0.5,
                 min_df=.01,
-                drop_features=drop_features,
+                features=features,
+                feature_mode="include",
                 include_bigrams=True,
-                name=f"DROP {drop_features} FEATS"
+                name=f"{features} FEATURES"
             )
             
             val_accs = model.get_validation_performance(n_folds=10, n_repeats=1)['accuracy']
@@ -49,28 +54,33 @@ def main() -> None:
             if cur_acc >= best_acc:
                 best_acc = cur_acc
                 standard_error = np.std(val_accs)/np.sqrt(len(val_accs))
+                stall_counter =0
+            else:
+                stall_counter += 1
 
             print(
                 f"{model.name}\t|\t" + \
                 f"{100*cur_acc:.2f}%\t\t" + \
                 f"{100*best_acc:.2f}%\t\t|\t" + \
+                #f"{stall_counter}"
                 f"{100*standard_error:.2f}%"
             )
 
-            if best_acc - cur_acc > 2*standard_error:
+            #if stall_counter == patience:
+            if best_acc - cur_acc > standard_error:
                 print("Exiting!")
                 break
 
-            drop_features += step_size
+            features += step_size
         except:
-            print(f"Exiting! (presumably, drop_features={drop_features} exceeds total number of features)")
+            print(f"Exiting! (presumably, features={features} exceeds total number of features)")
             break
     
-    plt.plot(np.arange(0, drop_features+step_size, step_size)[:len(all_accs)], all_accs, marker='s')
-    plt.xlabel("# Of Dropped Features")
+    plt.plot(np.arange(0, features+step_size, step_size)[:len(all_accs)], all_accs, marker='s')
+    plt.xlabel("# Of Features")
     plt.ylabel("Cross-Validation Accuracy")
     plt.grid(True)
-    plt.title("k-Backward Search Results")
+    plt.title("k-Forward Search Unigrams Results")
     plt.show()
 
 if __name__ == "__main__":

main.py
@@ -13,47 +13,111 @@ def main() -> None:
         #NaiveBayesClassifier(name="NaiveBayes(1952)", smoothing_alpha=1.0, n_features=train_X.shape[1]) # Uses all features
         #NaiveBayesClassifier(name="NaiveBayes(1933)", smoothing_alpha=1.0, n_features=1933), # Best unigram configuration (df_min = .005)
         #NaiveBayesClassifier(name="NaiveBayes(bigram, 3436)", smoothing_alpha=0.5, n_features=3436), # Best unigram+bigram configuration (df_min = .010)
+
+        RandomForestClassifier(
+            min_df=.01,
+            include_bigrams=False,
+            n_estimators=500,
+            name="RF Test"
+        ),]
+
+    """    # Found using backward search
         NaiveBayesClassifier(
             smoothing_alpha=1.0,
             min_df=.005,
-            drop_features=19,
+            features=565,
+            feature_mode="drop",
             include_bigrams=False,
-            name="NB ALL FEATURES"
+            name="UNIGRAM W/O 565 FEATS"
         ),
+        
+        # Found using forward search
         NaiveBayesClassifier(
             smoothing_alpha=1.0,
             min_df=.005,
-            drop_features=19,
+            features=101,
+            feature_mode="include",
             include_bigrams=False,
-            name="NB 1933 FEATURES"
+            name="UNIGRAM W/ 101 FEATS"
         ),
+        
+        # Found using grid search
+        NaiveBayesClassifier(
+            smoothing_alpha=1.0,
+            min_df=.01,
+            features=.4,
+            feature_mode="fraction",
+            include_bigrams=False,
+            name="UNIGRAM W/ .4 FEATS"
+        ),
+        
         NaiveBayesClassifier(
             smoothing_alpha=1.0,
             min_df=.005,
-            drop_features=122,
+            features=None,
             include_bigrams=False,
-            name="NB DROP 122 FEATS"
-        )
-    ]
+            name="UNIGRAM W/ ALL FEATS"
+        ),
+        
+        # Found using backward search
+        NaiveBayesClassifier(
+            smoothing_alpha=0.5,
+            min_df=.010,
+            features=102,
+            feature_mode="drop",
+            include_bigrams=True,
+            name="BIGRAM W/O 102 FEATS"
+        ),
+        
+        # Found using forward search
+        NaiveBayesClassifier(
+            smoothing_alpha=0.5,
+            min_df=.010,
+            features=147,
+            feature_mode="include",
+            include_bigrams=True,
+            name="BIGRAM W/ 147 FEATS"
+        ),
+        
+        # Found using grid search
+        NaiveBayesClassifier(
+            smoothing_alpha=0.5,
+            min_df=.01,
+            features=.8,
+            feature_mode="fraction",
+            include_bigrams=True,
+            name="BIGRAM W/ .8 FEATS"
+        ),
+        
+        NaiveBayesClassifier(
+            smoothing_alpha=0.5,
+            min_df=.010,
+            features=None,
+            include_bigrams=True,
+            name="BIGRAM W/ ALL FEATS"
+        ),
+    ]"""
 
     print("")
     print("CROSS-VALIDATION PERFORMANCES:")
     print("")
-    print("-"*100)
-    print(f"MODEL\t\t\t|\tACC\t|\tPREC\t|\tREC\t|\tF1")
-    print("-"*100)
+    print("-"*85)
+    print(f"{'MODEL':<25}|\tACC\t|\tPREC\t|\tREC\t|\tF1")
+    print("-"*85)
     for model in models:
         val_performance = model.get_validation_performance(n_folds=10, n_repeats=1)
 
         print(
-            f"{model.name}\t|\t" + \
+            f"{model.name:<25}|\t" + \
             f"{100*np.mean(val_performance['accuracy']):.2f}%\t|\t" + \
             f"{np.mean(val_performance['precision']):.2f}\t|\t" + \
             f"{np.mean(val_performance['recall']):.2f}\t|\t" + \
             f"{np.mean(val_performance['f1']):.2f}"
         )
+
     print("")
 
+    exit()
 
     # NOTE: we should only start looking at test set performance in a couple of weeks or so
     #   -> modelling/hyperparameter choices should NOT be based on test set performance 
@@ -61,20 +125,20 @@ def main() -> None:
     print("")
     print("TEST SET PERFORMANCES:")
     print("")
-    print("-"*100)
-    print(f"MODEL\t\t\t|\tACC\t|\tPREC\t|\tREC\t|\tF1")
-    print("-"*100)
+    print("-"*85)
+    print(f"{'MODEL':<25}|\tACC\t|\tPREC\t|\tREC\t|\tF1")
+    print("-"*85)
     for model in models:
         test_set_performance = model.get_test_performance()
         print(
-            f"{model.name}\t|\t" + \
+            f"{model.name:<25}|\t" + \
             f"{100*test_set_performance['accuracy']:.2f}%\t|\t" + \
             f"{test_set_performance['precision']:.3f}\t|\t" + \
             f"{test_set_performance['recall']:.3f}\t|\t" + \
             f"{test_set_performance['f1']:.3f}"
         )
     
-    models[0].analyse_feature_importances()
+    #models[-1].analyse_feature_importances()
 
 if __name__ == "__main__":
     main()

models.py
@@ -1,4 +1,4 @@
-from typing import Dict
+from typing import Dict, Literal
 
 import numpy as np
 
@@ -87,24 +87,34 @@ class Classifier:
         return self.get_performance_metrics(test_X, test_y)
 
 class NaiveBayesClassifier(Classifier):
-    def __init__(self, smoothing_alpha: float=1.0, drop_features: int=0, min_df: float=.005, include_bigrams: bool=False, name: str="NaiveBayes") -> None:
+    def __init__(self, 
+                 smoothing_alpha: float=1.0, 
+                 features: float=None, 
+                 feature_mode: Literal["include", "drop", "fraction"]="include",
+                 min_df: float=.005, 
+                 include_bigrams: bool=False, 
+                 name: str="NaiveBayes") -> None:
+        
         super().__init__(min_df=min_df, include_bigrams=include_bigrams, name=name)
         self.alpha = smoothing_alpha
         self.min_df = min_df
-        self.drop_features = drop_features
+        self.feature_param = features
+        self.feature_mode = feature_mode
+        if self.feature_mode not in ["include", "drop", "fraction"]:
+            raise ValueError(f"Feature mode {self.feature_mode} invalid (choose from ['include', 'drop', 'fraction')")
         self.include_bigrams = include_bigrams
 
     def _initialize_model(self) -> None:
         self.model = MultinomialNB(alpha=self.alpha, force_alpha=True)
 
-    def select_features(self, X: np.ndarray, y: np.ndarray) -> np.ndarray:
+    def _select_features_include(self, X: np.ndarray, y: np.ndarray) -> np.ndarray:
         # Return indices of features that should be used
-        if self.drop_features in [None, 0]:
+        if self.feature_param in [None, X.shape[1]]:
             self.feature_indices = np.arange(X.shape[1])
-        elif self.drop_features > X.shape[1]:
+        elif self.feature_param > X.shape[1]:
             raise ValueError(
-                f"Total number of possible features {X.shape[1]} exceeds " + \
-                f"specified number of features to drop {self.drop_features}"
+                f"Total number of possible features {X.shape[1]} lower than " + \
+                f"specified number of features to use {self.feature_param}"
             )
         else:
             binary_X = (X > 0)*1
@@ -114,9 +124,56 @@ class NaiveBayesClassifier(Classifier):
                 mi_scores[feature_idx] = mutual_info_score(binary_X[:,feature_idx], y)
 
             feature_score = mi_scores
-            n_features = len(feature_score) - self.drop_features
+
+            self.feature_indices = np.argpartition(feature_score, -self.feature_param)[-self.feature_param:]
+
+    def _select_features_drop(self, X: np.ndarray, y: np.ndarray) -> np.ndarray:
+        # Return indices of features that should be used
+        if self.feature_param in [None, 0]:
+            self.feature_indices = np.arange(X.shape[1])
+        elif self.feature_param > X.shape[1]:
+            raise ValueError(
+                f"Total number of possible features {X.shape[1]} exceeds " + \
+                f"specified number of features to drop {self.feature_param}"
+            )
+        else:
+            binary_X = (X > 0)*1
+
+            mi_scores = np.zeros(binary_X.shape[1])
+            for feature_idx in range(binary_X.shape[1]):
+                mi_scores[feature_idx] = mutual_info_score(binary_X[:,feature_idx], y)
+
+            feature_score = mi_scores
+            n_features = len(feature_score) - self.feature_param
 
             self.feature_indices = np.argpartition(feature_score, -n_features)[-n_features:]
+
+    def _select_features_fraction(self, X: np.ndarray, y: np.ndarray) -> np.ndarray:
+        # Return indices of features that should be used
+        if self.feature_param in [None, 1]:
+            self.feature_indices = np.arange(X.shape[1])
+        elif self.feature_param > 1 or self.feature_param <= 0:
+            raise ValueError(f"Fraction of features to use not in valid range <0, 1]")
+        else:
+            binary_X = (X > 0)*1
+
+            mi_scores = np.zeros(binary_X.shape[1])
+            for feature_idx in range(binary_X.shape[1]):
+                mi_scores[feature_idx] = mutual_info_score(binary_X[:,feature_idx], y)
+
+            feature_score = mi_scores
+            n_features = int(len(feature_score) * self.feature_param)
+
+            self.feature_indices = np.argpartition(feature_score, -n_features)[-n_features:]
+
+    def select_features(self, X: np.ndarray, y: np.ndarray) -> np.ndarray:
+        # Return indices of features that should be used
+        if self.feature_mode == 'include':
+            self._select_features_include(X, y)
+        elif self.feature_mode == 'drop':
+            self._select_features_drop(X, y)
+        elif self.feature_mode == 'fraction':
+            self._select_features_fraction(X, y)
         
     def get_validation_performance(self, n_folds: int=10, n_repeats: int=1) -> Dict[str,float]:
         # Load reviews from files
@@ -206,12 +263,15 @@ class NaiveBayesClassifier(Classifier):
             print(f"  {fn:<25} log-odds={lo:+.4f}  ")
 
 class RandomForestClassifier(Classifier):
-    def __init__(self, n_estimators: int = 100, name: str = "RandomForest") -> None:
-        super().__init__(name)
+    def __init__(self, n_estimators: int=100, min_df: float=.005, include_bigrams: bool=False, name: str="RandomForest") -> None:
+        super().__init__(min_df=min_df, include_bigrams=include_bigrams, name=name)
         self.n_estimators = n_estimators
+        self.min_df = min_df
+        self.include_bigrams = include_bigrams
+        self.name = name
 
     def _initialize_model(self) -> None:
-        self.model = SklearnRF(n_estimators=self.n_estimators, random_state=42)
+        self.model = SklearnRF(n_estimators=self.n_estimators, random_state=42, n_jobs=-1)
 
     def analyse_feature_importances(self, index_to_word_mapping: Dict[int,str], top_n: int = 20) -> None:
         importances = self.model.feature_importances_

nb_grid_search.py
@@ -22,35 +22,58 @@ def main() -> None:
             1.0,
             0.5,
             0.1
+        ],
+        "feature_fractions": [
+            .1, 
+            .2,
+            .3,
+            .4,
+            .5,
+            .6,
+            .7,
+            .8,
+            .9,
+            1
+            
         ]
     }
+
+    print("BIGRAMS")
+
+    best_acc = 0
+    best_model = " "*18
 
     print("")
     print("CROSS-VALIDATION PERFORMANCES:")
     print("")
-    print("-"*100)
-    print(f"MODEL\t\t\t|\tACC\t|\tPREC\t|\tREC\t|\tF1")
-    print("-"*100)
+    print("-"*90)
+    print(f"MODEL\t\t\t|\tACC\t|\t\tBEST\t\t|\tACC")
+    print("-"*90)
     for df in grid['doc_freqs']:
         for alpha in grid['alpha']:
-            # Specify which model configuration to evaluate
-            model = NaiveBayesClassifier(
-                smoothing_alpha=alpha,
-                min_df=df,
-                drop_features=0,
-                include_bigrams=False,
-                name=f"df_{df}_alpha_{alpha}"
-            )
+            for frac in grid["feature_fractions"]:
+                # Specify which model configuration to evaluate
+                model = NaiveBayesClassifier(
+                    smoothing_alpha=alpha,
+                    min_df=df,
+                    features=frac,
+                    feature_mode="fraction",
+                    include_bigrams=True,
+                    name=f"df_{df}_a_{alpha}_frac_{frac}"
+                )
 
-            val_performance = model.get_validation_performance(n_folds=10, n_repeats=2)
+                val_performance = model.get_validation_performance(n_folds=10, n_repeats=1)
 
-            print(
-                f"{model.name}\t|\t" + \
-                f"{100*np.mean(val_performance['accuracy']):.2f}%\t|\t" + \
-                f"{np.mean(val_performance['precision']):.2f}\t|\t" + \
-                f"{np.mean(val_performance['recall']):.2f}\t|\t" + \
-                f"{np.mean(val_performance['f1']):.2f}"
-            )
+                if np.mean(val_performance['accuracy']) > best_acc:
+                    best_acc = np.mean(val_performance['accuracy'])
+                    best_model = model.name
+
+                print(
+                    f"{model.name}\t|\t" + \
+                    f"{100*np.mean(val_performance['accuracy']):.2f}%\t|\t" + \
+                    f"{best_model}\t|\t" + \
+                    f"{100*best_acc:.2f}%"
+                )
 
 if __name__ == "__main__":
     main()